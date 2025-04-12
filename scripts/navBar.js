11b0dccaa2b1e01e694df1ff4ab1e87bbaedbb49
Matthew Clark <clarkd37@mcmaster.ca>
2025-04-12 15:34:21 -0400

    Navigation scripting

---
+++ scripts/navBar.js
@@ -0,0 +1,37 @@
+const home = document.getElementById("home");
+const about = document.getElementById("about");
+const research = document.getElementById("research");
+const development = document.getElementById("development");
+
+const homeButton = document.getElementById("home-button");
+const aboutButton = document.getElementById("about-button");
+const researchButton = document.getElementById("research-button");
+const developmentButton = document.getElementById("development-button");
+
+homeButton.onclick=()=>{
+    window.scroll({
+        top:0,
+        behavior:"smooth",
+    });
+}
+
+aboutButton.onclick=()=>{
+    window.scroll({
+        top: window.scrollY + about.getBoundingClientRect().top,
+        behavior:"smooth",
+    });
+}
+
+researchButton.onclick=()=>{
+    window.scroll({
+        top: window.scrollY + research.getBoundingClientRect().top,
+        behavior:"smooth",
+    });
+}
+
+developmentButton.onclick=()=>{
+    window.scroll({
+        top: window.scrollY + development.getBoundingClientRect().top,
+        behavior:"smooth",
+    });
+}
